d1103562e20554e48ab96b2a3ad05f1894ae7311
nishimurayoshinobu <h1202515004@hiratagakuen.onmicrosoft.com>
2025-10-23 12:30:25 +0900

    調べるボタンを追加

--- 泥棒ウサギ/Assets/NishimuraYoshinobu/N_Z_key/N_Z_key.cs
+++ 泥棒ウサギ/Assets/NishimuraYoshinobu/N_Z_key/N_Z_key.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class N_Z_key : MonoBehaviour
 {
     public string Z_key;
-    public float Z_Delay;//Zキーを押せる間隔
+    public float Z_Delay = 1f;//Zキーを押せる間隔
     bool inLnvestigate = false;//調べるためのフラグ
-    //public GameObject Z_keyPrefab;//Zキーのプレハブ
+    public GameObject Z_keyPrefab;//Zキーのプレハブ
     GameObject Z_keyOBJ;//Zキーのゲームオブジェクト
+
+    //オブジェクトの感知
+    public string Z_Perception;
 
     //調べる
     public void Lnvestigate()
@@ -16,7 +19,7 @@ public class N_Z_key : MonoBehaviour
         if (inLnvestigate == false)
         {
             inLnvestigate = true;//調べるフラグを立てる
-
+            Debug.Log("調べる");
             Invoke("StopLnvestigate",Z_Delay);//調べるの停止と、間隔追加
         }
     }
@@ -27,20 +30,39 @@ public class N_Z_key : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        if (Z_keyPrefab)
+        {
+            Z_keyOBJ = Instantiate(Z_keyPrefab);
+            
+        }
+        else
+        {
+            Z_keyOBJ = new GameObject("Z_keyOBJ");
+        }
         //調べるためのオブジェクトをプレイヤーの前に配置
-        Vector3 pos = transform.position;
+        //Vector3 pos = transform.position;
         Z_keyOBJ.transform.SetParent(transform);//調べるためのオブジェクトの親をプレイヤーに設定
+        Z_keyOBJ.transform.localPosition = new Vector3(0,0,-1);
     }
 
     // Update is called once per frame
     void Update()
     {
-       if(Input.GetButtonDown("Fire3"))
+       if(Input.GetButtonDown("Fire3") || Input.GetMouseButtonDown(0))
         {
+            Debug.Log("a");
             //調べるキーが押された
             Lnvestigate();
         }
         //調べるキーの回転と優先順位
         float inLnvestigate_Point_Z = -1;//調べるキーをプレイヤーの前にする
+        PlayerControll plmv = GetComponent<PlayerControll>();
+        if (plmv.angleZ > 30 && plmv.angleZ < 150)
+        {
+            //上向き
+            inLnvestigate_Point_Z = 1;//キャラクタの後ろにする
+        }
+        Z_keyOBJ.transform.position = 
+            new Vector3(transform.position.x, transform.position.y, inLnvestigate_Point_Z);
     }
 }
