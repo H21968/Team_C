7d9d2411c0f4456d6de5bf4ffef022f0e6cf3a59
nishimurayoshinobu <h1202515004@hiratagakuen.onmicrosoft.com>
2025-10-30 12:29:32 +0900

    人間に当たり判定とダメージ判定をいれました

--- 泥棒ウサギ/Assets/NishimuraYoshinobu/N_Human/N_Human_Behavior.cs
+++ 泥棒ウサギ/Assets/NishimuraYoshinobu/N_Human/N_Human_Behavior.cs
@@ -10,10 +10,10 @@ public class N_Human_Behavior : MonoBehaviour
     float Human_rectionDistance = 5.0f;//プレイヤーの感知
     Rigidbody2D rbody;            // Rigid body2
     Animator animator;            // Animator
-    //bool isMoving = false;        // 移動中フラグ
+    bool H1_Moving = false;        // 移動中フラグ
     float axisH;//横
     float axisV;//縦
-    public float damage;//プレイヤーに与えるダメージ量
+    public float Human1_damage = 3;//プレイヤーに与えるダメージ量
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -29,6 +29,7 @@ public class N_Human_Behavior : MonoBehaviour
 
         axisH = 0;
         axisV = 0;
+
         //playerのゲームオブジェクトを得る
         GameObject player = GameObject.FindGameObjectWithTag("player");
 
@@ -50,29 +51,29 @@ public class N_Human_Behavior : MonoBehaviour
                 float rad  = Mathf.Atan2(axisV, axisH);
                 float angle = rad* Mathf.Deg2Rad;
 
-                     //移動角度でアニメーションを変更する
-                     int  direction ;
+                //移動角度でアニメーションを変更する
+                int H_1direction;
                      if (angle >= -45 && angle < 45)
                     {
-                        //右向き
-                        direction = 3;
+                    //右向き
+                    H_1direction = 3;
                     }
                     else if (angle >= 45 && angle <= 135)
                     {
-                        //上向き
-                        direction = 2;
+                    //上向き
+                    H_1direction = 2;
                     }
                     else if (angle >= -135 && angle <= -45)
                     {
-                        //下向き
-                        direction = 0;
+                    //下向き
+                    H_1direction = 0;
                     }
                     else
                     {
-                        //左向き
-                        direction = 1;
+                    //左向き
+                    H_1direction = 1;
                     }
-                   // animator.SetInteger("Direction", direction);
+                     animator.SetInteger("H_1Direction", H_1direction);
                     //ベクトル
                     axisH = Mathf.Cos(rad)*Human_speed;
             }
@@ -92,7 +93,7 @@ public class N_Human_Behavior : MonoBehaviour
             PlayerControll player = collision.gameObject.GetComponent<PlayerControll>();
             if (player != null)
             {
-                PlayerControll.player_hp -= ((int)damage);
+                PlayerControll.player_hp -= ((int)Human1_damage);
                 Debug.Log("プレイヤーがダメージを受ける");
 
                 if (PlayerControll.player_hp <= 0)
