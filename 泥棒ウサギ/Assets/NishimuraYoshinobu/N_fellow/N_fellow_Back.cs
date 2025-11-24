3d64fef89a5a38468b7f8c7b268b1441336b6874
DESKTOP-O4QCOL4\Yo4no <h1202515004@hiratagakuen.onmicrosoft.com>
2025-11-24 20:36:23 +0900

    エネミーに生じていたアニメーションが機能しないバグを直しました

---
+++ 泥棒ウサギ/Assets/NishimuraYoshinobu/N_fellow/N_fellow_Back.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class N_fellow_Back : MonoBehaviour
+{
+    public float speed = 3.0f;    // 移動スピード
+    //int direction = 0;            // 移動方向
+    //float axisH;                  // 横軸
+    //float axisV;                  // 縦軸
+    public float angleZ = -90.0f; // 回転角度 プレイヤーの向き
+    Rigidbody2D rbody;            // Rigid body2
+    Animator animator;            // Animator
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>(); // Rigid body2Dを得る
+        animator = GetComponent<Animator>(); // Animatorを得る
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Direction_Back(Vector2 dir)
+    {
+        float rad = Mathf.Atan2(dir.y, dir.x);
+        float angle2 = rad * Mathf.Rad2Deg;
+
+        int Distinct2;
+        if (angle2 >= -45 && angle2 < 45)
+        {
+            //右向き
+            Distinct2 = 3;
+        }
+        else if (angle2 >= 45 && angle2 <= 135)
+        {
+            //上向き
+            Distinct2 = 2;
+        }
+        else if (angle2 >= -135 && angle2 <= -45)
+        {
+            //下向き
+            Distinct2 = 0;
+        }
+        else
+        {
+            //左向き
+            Distinct2 = 1;
+        }
+        animator.SetInteger("Distinct", Distinct2);
+    }
+}
+
